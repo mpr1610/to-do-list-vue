057257ffc80bb8fa72ec5067aaa1c2e2e01a4add
mrosa16 <michael.rosa@holu.com.br>
2023-12-20 23:51:03 -0300

    To do list making in vue js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Tarefas</h1>
+    <NewTask @taskAdd="addTask" />
+    <TaskGrid
+      @taskDeleted="deleteTask"
+      @taskStateChanged="toggleTaskState"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewTask from "./components/NewTask.vue";
+import TaskGrid from "./components/TaskGrid.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  components: { TaskGrid, NewTask },
+  methods: {
+    addTask(task) {
+      const sameName = (t) => t.name === task.name;
+      const reallyNew = this.tasks.filter(sameName).length == 0;
+      if (reallyNew) {
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true,
+        });
+      }
+    },
+    deleteTask(i) {
+      this.tasks.splice(i, 1);
+    },
+
+    toggleTaskState(i) {
+      this.tasks[i].pending = !this.tasks[i].pending;
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  font-family: "Courier New", Courier, monospace;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
 }
 </style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tasks-grid">
+    <template v-if="tasks.length">
+      <ComponentsTask
+        v-for="(task, i) in tasks"
+        :key="task.name"
+        :task="task"
+        @taskDeleted="$emit('taskDeleted', i)"
+        @taskStateChanged="$emit('taskStateChanged', i)"
+      ></ComponentsTask>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
+  </div>
+</template>
+
+<script>
+import ComponentsTask from "./ComponentsTask.vue";
+
+export default {
+  props: {
+    tasks: { type: Array, required: true },
+  },
+  components: { ComponentsTask },
+};
+</script>
+
+<style scoped>
+.tasks-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.tasks-grid .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
+</style>
